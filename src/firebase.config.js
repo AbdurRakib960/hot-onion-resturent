59c5a97b5ae1f0b911513247661c7215443b825f
Abdur Rakib <rakibabdur960@gmail.com>
2020-09-25 02:05:28 +0600

    final commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnZ8sSPVYMffaniZ8rIB9AcofmrbJwCXo",
+  authDomain: "fire-auth-efcca.firebaseapp.com",
+  databaseURL: "https://fire-auth-efcca.firebaseio.com",
+  projectId: "fire-auth-efcca",
+  storageBucket: "fire-auth-efcca.appspot.com",
+  messagingSenderId: "317083518466",
+  appId: "1:317083518466:web:2a8a772da33dfdadd6c91b"
+};
+  export default firebaseConfig;
